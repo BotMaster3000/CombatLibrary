9c69437e674a2527dfad770663efc2fb787c1dd8
BotMaster3000 <jdgebauer@gmx.de>
2018-09-23 15:31:38 +0200

    Added Pre-Defined Location
    Added Pre-Defined Location "Grassland"

---
+++ CombatLibrary/EntityObjects/Locations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CombatLibrary.Entities;
+
+namespace CombatLibrary.EntityObjects
+{
+    public static class Locations
+    {
+        public static Location Grassland()
+        {
+            return new Location("Grassland", "You see wide fields of grass");
+        }
+    }
+}
